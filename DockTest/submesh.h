d211ae772f1a03851fdaeefed4e500d1abe2397e
Jordi Oña <jordior97@gmail.com>
2019-03-21 11:56:21 +0100

    Implemented submesh and vertexformat classes

---
+++ DockTest/submesh.h
@@ -0,0 +1,35 @@
+#ifndef SUBMESH_H
+#define SUBMESH_H
+
+#include"vertexformat.h"
+#include<QOpenGLBuffer>
+#include<QOpenGLVertexArrayObject>
+
+
+class SubMesh
+{
+public:
+    SubMesh();
+    SubMesh(VertexFormat vFormat, void *data, int size);
+    SubMesh(VertexFormat vFormat, void *data, int size, unsigned int *indices, int indices_count);
+
+    void Update();
+    void Draw();
+    void Destroy();
+
+private:
+    unsigned char *data = nullptr;
+    size_t dataSize = 0;
+
+    unsigned int *indices = nullptr;
+    size_t indexCount = 0;
+
+    VertexFormat vertexFormat;
+    QOpenGLBuffer vbo;
+    QOpenGLBuffer ibo;
+    QOpenGLVertexArrayObject vao;
+
+
+};
+
+#endif // SUBMESH_H
---
+++ DockTest/vertexformat.h
@@ -0,0 +1,30 @@
+#ifndef VERTEXFORMAT_H
+#define VERTEXFORMAT_H
+
+#define MAX_VERTEX_ATTRIBUTES 5
+
+struct VertexAttribute
+{
+    bool enabled = false;
+    int offset = 0;
+    int ncomp = 0;
+};
+
+class VertexFormat
+{
+
+public:
+    void SetVertexAttribute(int location, int offset, int ncomp)
+    {
+        attribute[location].enabled = true;
+        attribute[location].offset = offset;
+        attribute[location].ncomp = ncomp;
+        size += ncomp * sizeof(float);
+    }
+
+    VertexAttribute attribute[MAX_VERTEX_ATTRIBUTES];
+    int size = 0;
+
+};
+
+#endif // VERTEXFORMAT_H
